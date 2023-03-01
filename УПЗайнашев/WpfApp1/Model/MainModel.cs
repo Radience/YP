881ea97dcb5ec6f0fdc8ee176ba6889f4d4d3f78
= <=>
2023-03-01 11:10:23 +0300

    1

---
+++ УПЗайнашев/WpfApp1/Model/MainModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    class MainModel
+    {
+        private static SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-QILM6OR\SQLEXPRESS;Initial Catalog=YPZainashev;Integrated Security=True");
+        private static DataTable dataTable = null;
+        private static SqlDataAdapter adapter = null;
+
+        private static void Acccounts()
+        {
+            adapter = new SqlDataAdapter($"SELECT * FROM Accounts", connection);
+            dataTable = new DataTable();
+            adapter.Fill(dataTable);
+        }
+
+        public static bool SignIn(string login, string password)
+        {
+            Acccounts();
+            foreach(DataRow row in dataTable.Rows)
+            {
+                if (row["login"].ToString() == login && row["password"].ToString() == password)
+                {
+                    Role.role = row["id_role"].ToString();
+                    return true;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ УПЗайнашев/WpfApp1/View/Authoriz.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Authoriz.xaml
+    /// </summary>
+    public partial class Authoriz : Page
+    {
+        public Authoriz()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
